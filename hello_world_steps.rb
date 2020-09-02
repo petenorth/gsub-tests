4d5928efc637fd176db628da176665de0671fb17
Peter Fry <peterfry@peters-mbp-2.mynet>
2020-09-02 19:28:36 +0100

    steps

---
+++ hello_world_steps.rb
@@ -0,0 +1,11 @@
+Given('I have a Rails project') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I write a Hello World Cucumber test') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then('I should be able to run it and see {string} printed on screen') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
