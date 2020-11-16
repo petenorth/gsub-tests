6c408633f0b8838eb6ef89524b663436c530a77a
Peter Fry <peterfry@peters-mbp-2.mynet>
2020-11-16 22:02:28 +0000

    remove ruby version requirement

--- hello_world_steps.rb
+++ hello_world_steps.rb
@@ -1,10 +1,3 @@
-Given("I am on a/the {string} page") do |string|
-  path_for(string).tap do |p|
-    visit(p)
-    expect(page.current_path).to eql(p)
-  end
-end
-
 Given('I have a Rails project') do
   puts "Given...."
 end
