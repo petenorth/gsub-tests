9704f7f410bf327a68ec63f7ab8f7e4e6c32e25f
Peter Fry <peterfry@peters-mbp-2.mynet>
2020-11-16 22:24:44 +0000

    remove ruby version requirement

--- hello_world_steps.rb
+++ hello_world_steps.rb
@@ -3,7 +3,7 @@ Given('I have a Rails project') do
 end
 
 When('I write a Hello World Cucumber test') do
-  visit "/"
+  visit "http://en.wikipedia.org/wiki/Baltimore_Ravens"
 end
 
 Then('I should be able to run it and see {string} printed on screen') do |string|
